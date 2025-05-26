5d5b28fbcbb7dc93f8fd6248d9acee29ca8a6b64
Roberto <robertodepaula1937@gmail.com>
2025-05-26 13:25:35 -0300

    inplementado a propriedade disable, visibilidade de botoes e função cadastro

--- src/app/componente1/componente1.component.ts
+++ src/app/componente1/componente1.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Pessoa } from '../model/Pessoa';
 
 @Component({
   selector: 'app-componente1',
@@ -9,8 +10,28 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class Componente1Component {
   Formulario = new FormGroup({
-    nome: new FormControl('',   [Validators.required,   Validators.minLength(3)]),
-    idade: new FormControl(null,[Validators.required, Validators.min(0),Validators.max(120)]),
-    cidade:new FormControl('',  [Validators.required,   Validators.min(0),Validators.max(120)])
+    Nome: new FormControl('',   [Validators.required,   Validators.minLength(3)]),
+    Idade: new FormControl(null,[Validators.required, Validators.min(0),Validators.max(120)]),
+    Cidade:new FormControl('',  [Validators.required,   Validators.min(0),Validators.max(120)])
   })
+  //visibilidade dos botoes
+  bntCadastrar:boolean=true;
+
+  //Vetor para armazenamento dos dados
+  vetor:Pessoa[]=[]
+
+  //função para cadastro
+  cadastrar(){
+
+    //cadastrar no vetor
+    this.vetor.push(this.Formulario.value as Pessoa);
+
+    //Linpar os dados
+    this.Formulario.reset();
+
+    //visualização via console
+    console.table(this.vetor)
+  }
+
+
 }
